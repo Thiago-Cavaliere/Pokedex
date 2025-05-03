1ef2a1ffa3a27afd7d6f606eec0d02cdbd2838c4
thiagocavaliere1234@gmail.com <thiagocavaliere1234@gmail.com>
2025-05-03 17:00:48 -0300

    adc icon

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer>
       <div className="footer__container">
-        <p className="footer__text">Desenvolvido por Thiago Cavaliére.</p>
+        <p className="footer__text">Desenvolvido por CΛVΛLIΞRE.</p>
         <p className="footer__text">Pokédex - Todos os direitos reservados -</p>
         <p className="footer__text">2025.</p>
       </div>
